2189c18b333c995332f80f0bfbc7515f79e21d0e
kmk <aksrl132@gmail.com>
2023-08-28 09:45:04 +0900

    20230825

--- test_zone/flask_html.py
+++ test_zone/flask_html.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template('map_out.html')
 
+
+# 챗봇 엔진 query 전송 API
+#@app.route('/load_wind', methods=['GET'])
+@app.route('/load_wind', methods=['GET'])
+def load_wind():
+    return render_template('wind_out.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
